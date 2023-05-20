282d7390863de83a1b8043b2addb08e94d54ddd3
Gustavords92 <gustavords92@gmail.com>
2023-05-19 21:00:48 -0400

    Organize and refactor form.eventlistener method invocation

--- js/script.js
+++ js/script.js
@@ -12,11 +12,20 @@ Book.prototype.info = function () {
 }
 
 
-function addBookToLibrary(book) {
+function addBookToLibrary(form) {
+    //for testing
+    // console.log(form.querySelector('input[name="title"]').value);
+
+    const formData = new FormData(form);
+    const book = new Book(
+        formData.get("title"),
+        formData.get("author"),
+        formData.get("pages"),
+        formData.get("read")
+    );
     // do stuff here
     myLibrary.push(book);
 }
-
 
 /**
  * TODO: get entries of object to display all (even if i add later on)
@@ -43,30 +52,12 @@ let string = ``;
 form.addEventListener(`submit`, (x) => {
     //so form doesn't mess up
     x.preventDefault();
-
-    console.log(form.querySelector('input[name="title"]').value);
-    // console.log(x);
-
-    const formData = new FormData(form);
-    const book = new Book(
-        formData.get("title"),
-        formData.get("author"),
-        formData.get("pages"),
-        formData.get("read")
-    );
-
-    addBookToLibrary(book);
-
-
-    // info.innerText += "\n" + book.info() + `  :: index -> ${myLibrary.length-1}`;
-    // info.innerText = ``;
-    // myLibrary.forEach((book) => {
-    //     // console.log(book.info() + "\n")
-    //     info.innerText += book.info() + "\n";
-    // });
-    // console.log(myLibrary);
-
+    addBookToLibrary(form);
     displayBook();
+
+    
+    //for testing
+    // console.log(form.querySelector('input[name="title"]').value);
 });
 
 form.addEventListener("formdata", (e) => {
